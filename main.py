3774bc184a9b941b13f80cd7d4562e94525c61ad
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-03-01 14:49:39 -0600

    updated topic name

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from waggle.plugin import Plugin
 def main():
     with Plugin() as plugin:
         print("publishing hello world!")
-        plugin.publish("test.hello_world", "hello world")
+        plugin.publish("tutorial.hello_world", "hello world")
 
 
 if __name__ == "__main__":
